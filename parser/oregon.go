0066461fdb495f31e6451cb6c9333e920c4ea41b
Geoff Holden <geoff@geoffholden.com>
2015-07-26 08:32:36 -0230

    Adds additional OS sensors and a test case.

--- parser/oregon.go
+++ parser/oregon.go
@@ -8,13 +8,45 @@ type Oregon struct {
 }
 
 func init() {
-	RegisterSensor("OS3", new(Oregon))
+	var o Oregon
+	RegisterSensor("OS3", &o)
+	RegisterSensor("OS2", &o)
 }
 
 func (d *Oregon) Parse(data string) {
 	fmt.Println("Sensor ID", data[0:4], "Channel", data[4:5], "Rolling Code", data[5:7], "Flags", data[7:8])
 	switch data[0:4] {
-	case "1984":
+	case "1D20", "F824", "F8B4":
+		temperature := float32(data[10]-'0') * 10.0
+		temperature += float32(data[9]-'0') * 1.0
+		temperature += float32(data[8]-'0') * 0.1
+		if data[11] != '0' {
+			temperature *= -1
+		}
+
+		humidity := float32(data[13]-'0') * 10.0
+		humidity += float32(data[12]-'0') * 1.0
+		fmt.Println("Temperature", temperature, "Humidity", humidity)
+	case "EC40", "C844":
+		temperature := float32(data[10]-'0') * 10.0
+		temperature += float32(data[9]-'0') * 1.0
+		temperature += float32(data[8]-'0') * 0.1
+		if data[11] != '0' {
+			temperature *= -1
+		}
+
+		fmt.Println("Temperature", temperature)
+	case "EC70":
+		uv := (data[9] - '0') * 10
+		uv += (data[8] - '0') * 1
+
+		fmt.Println("UV", uv)
+	case "D874":
+		uv := (data[12] - '0') * 10
+		uv += (data[11] - '0') * 1
+
+		fmt.Println("UV", uv)
+	case "1984", "1994":
 		dir, _ := strconv.ParseInt(data[8:9], 16, 8)
 		direction := float32(dir) * 22.5
 
@@ -31,6 +63,7 @@ func (d *Oregon) Parse(data string) {
 		rate += float32(data[10]-'0') * 1.0
 		rate += float32(data[9]-'0') * 0.10
 		rate += float32(data[8]-'0') * 0.01
+		rate *= 25.4
 
 		total := float32(data[17]-'0') * 100.0
 		total += float32(data[16]-'0') * 10.0
@@ -38,9 +71,22 @@ func (d *Oregon) Parse(data string) {
 		total += float32(data[14]-'0') * 0.100
 		total += float32(data[13]-'0') * 0.010
 		total += float32(data[12]-'0') * 0.001
+		total *= 25.4
 
 		fmt.Println("Rain Rate", rate, "Total Rain", total)
-	case "F824":
+	case "2D10":
+		rate := float32(data[10]-'0') * 10.0
+		rate += float32(data[9]-'0') * 1.0
+		rate += float32(data[8]-'0') * 0.1
+
+		total := float32(data[15]-'0') * 1000.0
+		total += float32(data[14]-'0') * 100.0
+		total += float32(data[13]-'0') * 10.0
+		total += float32(data[12]-'0') * 1.0
+		total += float32(data[11]-'0') * 0.1
+
+		fmt.Println("Rain Rate", rate, "Total Rain", total)
+	case "5D60":
 		temperature := float32(data[10]-'0') * 10.0
 		temperature += float32(data[9]-'0') * 1.0
 		temperature += float32(data[8]-'0') * 0.1
@@ -50,7 +96,36 @@ func (d *Oregon) Parse(data string) {
 
 		humidity := float32(data[13]-'0') * 10.0
 		humidity += float32(data[12]-'0') * 1.0
-		fmt.Println("Temperature", temperature, "Humidity", humidity)
+
+		var comfort string
+		switch data[14] {
+		case '0':
+			comfort = "Normal"
+		case '4':
+			comfort = "Comfortable"
+		case '8':
+			comfort = "Dry"
+		case 'C':
+			comfort = "Wet"
+		}
+
+		pressure := 856
+		pressure += int((data[16] - '0') * 10)
+		pressure += int((data[15] - '0') * 1)
+
+		var forecast string
+		switch data[17] {
+		case '2':
+			forecast = "Cloudy"
+		case '3':
+			forecast = "Rainy"
+		case '6':
+			forecast = "Partly Cloudy"
+		case 'C':
+			forecast = "Sunny"
+		}
+
+		fmt.Println("Temperature", temperature, "Humidity", humidity, "Comfort", comfort, "Pressure", pressure, "Forecast", forecast)
 	default:
 		fmt.Println("Oregon Sensor", data[0:4])
 	}
---
+++ parser/oregon_test.go
@@ -0,0 +1,12 @@
+package parser
+
+func ExampleParseTHGR122NX() {
+	var o Oregon
+	o.Parse("1D20485C480882835")
+	o.Parse("1D2016B1091073A14")
+	// Output:
+	// Sensor ID 1D20 Channel 4 Rolling Code 85 Flags C
+	// Temperature -8.4 Humidity 28
+	// Sensor ID 1D20 Channel 1 Rolling Code 6B Flags 1
+	// Temperature 19 Humidity 37
+}
